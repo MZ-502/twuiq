55a7bfa292bd6399d3659b270efdd18c71170ad1
MZ-502 <127432603+MZ-502@users.noreply.github.com>
2026-03-05 01:34:05 +0300

    Add files via upload

---
+++ two_numbers_operations.py
@@ -0,0 +1,10 @@
+# العمليات على رقمين
+num1 = float(input("ادخل الرقم الاول: "))
+num2 = float(input("ادخل الرقم الثاني: "))
+print("المجموع =", num1 + num2)
+print("الطرح =", num1 - num2)
+print("الضرب =", num1 * num2)
+if num2 != 0:
+    print("القسمة =", num1 / num2)
+else:
+    print("لا يمكن القسمة على صفر")
